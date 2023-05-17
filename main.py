93d6f4ad6127d82717e98891db85a47d8b9372b2
Aya-mounir <aya.mounir167@gmail.com>
2023-05-17 13:41:22 +0300

    alphabeta and minimax

--- main.py
+++ main.py
@@ -1,16 +1,271 @@
-
+from pygame.locals import *
+import os
 import numpy as np
+import random
+import pygame
+import sys
+import math
 
+white = (255, 255, 255)
+black = (0, 0, 0)
+gray = (75, 75, 75)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
 
 rowCnt = 6
 columnCnt = 7
-
-computer = 0
+PLAYER = 0
 AI = 1
+EMPTY = 0
+PLAYER_PIECE = 1
+AI_PIECE = 2
+windowLen = 4
+screenSize = 100
 
-def createBoard():
+
+def alphabeta(board, depth, alpha, beta, maximizingPlayer):
+    valid_locations = get_valid_locations(board)
+    is_terminal = is_terminal_node(board)
+    if depth == 0 or is_terminal:
+        if is_terminal:
+            if winning_move(board, AI_PIECE):
+                return (None, 100000000000000)
+            elif winning_move(board, PLAYER_PIECE):
+                return (None, -10000000000000)
+            else:  # Game is over, no more valid moves
+                return (None, 0)
+        else:  # Depth is zero
+            return (None, score_position(board, AI_PIECE))
+    if maximizingPlayer:
+        value = -math.inf
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            b_copy = board.copy()
+            drop_piece(b_copy, row, col, AI_PIECE)
+            new_score = alphabeta(b_copy, depth - 1, alpha, beta, False)[1]
+            if new_score > value:
+                value = new_score
+                column = col
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return column, value
+
+    else:  # Minimizing player
+        value = math.inf
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            b_copy = board.copy()
+            drop_piece(b_copy, row, col, PLAYER_PIECE)
+            new_score = alphabeta(b_copy, depth - 1, alpha, beta, True)[1]
+            if new_score < value:
+                value = new_score
+                column = col
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return column, value
+
+
+def create_board():
     board = np.zeros((rowCnt, columnCnt))
     return board
 
-def dropCheckers(board, row, column, checker):
-    board[row][column] = checker
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+def is_valid_location(board, col):
+    return board[rowCnt - 1][col] == 0
+
+
+def get_next_open_row(board, col):
+    for r in range(rowCnt):
+        if board[r][col] == 0:
+            return r
+
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(columnCnt - 3):
+        for r in range(rowCnt):
+            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
+                c + 3] == piece:
+                return True
+
+    # Check vertical locations for win
+    for c in range(columnCnt):
+        for r in range(rowCnt - 3):
+            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
+                c] == piece:
+                return True
+
+    # Check positively sloped diaganols
+    for c in range(columnCnt - 3):
+        for r in range(rowCnt - 3):
+            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][
+                c + 3] == piece:
+                return True
+
+    # Check negatively sloped diaganols
+    for c in range(columnCnt - 3):
+        for r in range(3, rowCnt):
+            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][
+                c + 3] == piece:
+                return True
+
+
+def evaluate_window(window, piece):
+    score = 0
+    opp_piece = PLAYER_PIECE
+    if piece == PLAYER_PIECE:
+        opp_piece = AI_PIECE
+
+    if window.count(piece) == 4:
+        score += 100
+    elif window.count(piece) == 3 and window.count(0) == 1:
+        score += 5
+    elif window.count(piece) == 2 and window.count(0) == 2:
+        score += 2
+
+    if window.count(opp_piece) == 3 and window.count(0) == 1:
+        score -= 4
+
+    return score
+
+
+def score_position(board, piece):
+    score = 0
+
+    ## Score center column
+    center_array = [int(i) for i in list(board[:, columnCnt // 2])]
+    center_count = center_array.count(piece)
+    score += center_count * 3
+
+    ## Score Horizontal
+    for r in range(rowCnt):
+        row_array = [int(i) for i in list(board[r, :])]
+        for c in range(columnCnt - 3):
+            window = row_array[c:c + windowLen]
+            score += evaluate_window(window, piece)
+
+    ## Score Vertical
+    for c in range(columnCnt):
+        col_array = [int(i) for i in list(board[:, c])]
+        for r in range(rowCnt - 3):
+            window = col_array[r:r + windowLen]
+            score += evaluate_window(window, piece)
+
+    ## Score posiive sloped diagonal
+    for r in range(rowCnt - 3):
+        for c in range(columnCnt - 3):
+            window = [board[r + i][c + i] for i in range(windowLen)]
+            score += evaluate_window(window, piece)
+
+    for r in range(rowCnt - 3):
+        for c in range(columnCnt - 3):
+            window = [board[r + 3 - i][c + i] for i in range(windowLen)]
+            score += evaluate_window(window, piece)
+
+    return score
+
+
+def is_terminal_node(board):
+    return winning_move(board, PLAYER_PIECE) or winning_move(board, AI_PIECE) or len(get_valid_locations(board)) == 0
+
+
+def minimax(board, depth, maximizingPlayer):
+    valid_locations = get_valid_locations(board)
+    is_terminal = is_terminal_node(board)
+    if depth == 0 or is_terminal:
+        if is_terminal:
+            if winning_move(board, AI_PIECE):
+                return (None, 100000000000000)
+            elif winning_move(board, PLAYER_PIECE):
+                return (None, -10000000000000)
+            else:  # Game is over, no more valid moves
+                return (None, 0)
+        else:  # Depth is zero
+            return (None, score_position(board, AI_PIECE))
+    if maximizingPlayer:
+        value = -math.inf
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            b_copy = board.copy()
+            drop_piece(b_copy, row, col, AI_PIECE)
+            new_score = minimax(b_copy, depth - 1, False)[1]
+            if new_score > value:
+                value = new_score
+                column = col
+        return column, value
+
+    else:  # Minimizing player
+        value = math.inf
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            b_copy = board.copy()
+            drop_piece(b_copy, row, col, PLAYER_PIECE)
+            new_score = minimax(b_copy, depth - 1, True)[1]
+            if new_score < value:
+                value = new_score
+                column = col
+        return column, value
+
+
+
+board = create_board()
+print_board(board)
+pygame.init()
+width = columnCnt * screenSize
+height = (rowCnt + 1) * screenSize
+
+size = (width, height)
+
+RADIUS = int(screenSize / 2 - 5)
+
+screen = pygame.display.set_mode(size)
+draw_board(board)
+pygame.display.update()
+
+myfont = pygame.font.SysFont("connect 4", 75)
+
+
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+# Game Resolution
+screen_width = 700
+screen_height = 700
+# screen=pygame.display.set_mode((screen_width, screen_height))
+
+
+# Game Fonts
+font = "Montserrat-Bold.ttf"
+clock = pygame.time.Clock()
+FPS = 30
+
+
+def text_format(message, textFont, textSize, textColor):
+    newFont = pygame.font.Font(textFont, textSize)
+    newText = newFont.render(message, 0, textColor)
+    return newText
+
+
+opsi_setting = "MINIMAX"
+
+
+
+
+mainmenu(opsi_setting)
+pygame.quit()
+QUIT()
